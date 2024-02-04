4e4c4f2a3d5423822cbf4704b38e8980e9a982f4
Shaanukstar123 <shaanukag0@gmail.com>
2024-02-04 06:36:09 +0000

    integrated summariser

--- newsAnalysis.py
+++ newsAnalysis.py
@@ -1,7 +1,25 @@
+import json
+import os
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-import json, os
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import requests
+from summariser.summariser import summarise_text
+
+def update_json_with_summaries(articles):
+    """
+    Update the list of articles with summaries.
+    """
+    futures = []
+    with ThreadPoolExecutor(max_workers=3) as executor:  # threading agents
+        for article in articles:
+            futures.append(executor.submit(summarise_text, article['content']))
+            print(article['content'])
+
+        for future, article in zip(as_completed(futures), articles):
+            print(future.result())
+            article['summary'] = future.result()
 
 llm = ChatOpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'), model="gpt-3.5-turbo-0125")
 
@@ -26,6 +44,10 @@ filteredIndexes = [int(article["id"]) for article in filteredList]
 print(filteredIndexes)
 filteredInputList = [article for article in input if article["id"] in filteredIndexes]
 print(len(filteredInputList))
+with open('filtered_output.json', 'r', encoding='utf-8') as file:
+    filtered_articles = json.load(file)
+update_json_with_summaries(filtered_articles)
+
 filteredInput = json.dumps(filteredInputList)
 
 prompt = ChatPromptTemplate.from_messages([
